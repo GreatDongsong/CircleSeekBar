14543aed16dd5657c36610c8f4eab805382d8729
Dawson.Gao <dawson.gao@libratone.com.cn>
2019-02-21 18:59:13 +0800

    add screenshot

--- app/src/main/java/com/dawson/circleseekbar/MainActivity.java
+++ app/src/main/java/com/dawson/circleseekbar/MainActivity.java
@@ -1,8 +1,10 @@
 package com.dawson.circleseekbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CircularContinuousDotSeekBar circularContinuousDotSeekBar = findViewById(R.id.cs);
+        final TextView textView = findViewById(R.id.textView2);
+        circularContinuousDotSeekBar.setProgressChangeListener(new CircularContinuousDotSeekBar.OnProgressControlListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onProgressChange(View view, int progress) {
+                textView.setText(String.format("%d", progress));
+            }
+
+            @Override
+            public void onProgressControlStart(View view) {
+
+            }
+
+            @Override
+            public void onProgressControlEnd(View view) {
+
+            }
+        });
         circularContinuousDotSeekBar.setProgress(50);
     }
 }
